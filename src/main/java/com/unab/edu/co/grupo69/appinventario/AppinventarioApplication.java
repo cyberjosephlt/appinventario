64d44d276eb1c8c1c6450910136a00f2789cb0cb
José Luis Lizalda Torres <lizaldatorres@gmail.com>
2022-11-23 21:02:12 -0500

    INICIALIZACIÓN DEL PROYECTO CON LA CONFIGURACIÓN BÁSICA Y LAS DEPENDENCIAS NECESARIAS

---
+++ src/main/java/com/unab/edu/co/grupo69/appinventario/AppinventarioApplication.java
@@ -0,0 +1,13 @@
+package com.unab.edu.co.grupo69.appinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppinventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppinventarioApplication.class, args);
+	}
+
+}
